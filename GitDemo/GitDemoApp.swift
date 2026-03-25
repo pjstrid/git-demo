1a8dbc88c230d8e9024ffed077f1354eecd85d47
jonathan strid <pjstrid@outlook.com>
2026-03-25 10:42:52 +0100

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Jonathan Strid on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
